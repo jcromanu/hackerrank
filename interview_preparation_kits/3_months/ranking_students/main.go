a8917f361c32484a9dbc315e65b47089de3bab61
jckhan <jckhan@Mac-mini-de-jckhan.local>
2023-11-02 17:01:57 -0600

    [3_month_interview] grading students

---
+++ interview_preparation_kits/3_months/ranking_students/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+func gradingStudents(grades []int32) []int32 {
+    //range all grades
+    //if grade is less than 38 do nothing 
+    //for each grade find the next multiple of 5
+    //find the difference and compare if it is less than 3
+    //if it is less than 3 round to the next multiple
+    var temp int32 
+    for i , v := range grades {
+        if v >= 38{
+            temp = v+1
+            for{ 
+                if temp % 5==0  {
+                    if temp-v < 3 {
+                    grades[i] = temp
+                    break
+                    }
+                    break 
+                }
+                temp++
+            }
+        }
+    }
+    return grades 
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    gradesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    var grades []int32
+
+    for i := 0; i < int(gradesCount); i++ {
+        gradesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+        checkError(err)
+        gradesItem := int32(gradesItemTemp)
+        grades = append(grades, gradesItem)
+    }
+
+    result := gradingStudents(grades)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, "\n")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
